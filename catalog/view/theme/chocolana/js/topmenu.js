7a43896b38ad1d6fbcfc9c0d87cf52dc5836fcb6
dfox <dfox@foxartbox.com>
2023-05-06 22:03:48 +0300

    JS ref

--- catalog/view/theme/chocolana/js/topmenu.js
+++ catalog/view/theme/chocolana/js/topmenu.js
@@ -1,68 +1,61 @@
-var $ = jQuery;
-
+var $ = jQuery
 
 $(window).on('load', function () {
+	const $html = $('html')
+
 	// show menu on hover at menu button
 	// only on desktop, cause in other way click will not work
 	if ( !isMobile ) {
 		$('.menu__toggle').mouseover(function() {
-			$('html').addClass('topmenu__active');
-		});
+			$('html').addClass('topmenu__active')
+		})
 	}
 
-
-
-	$(window).resize();
+	$(window).resize()
 
 	$(window).scroll(function() {
-		menu_top__show_on_mouse_scroll();
-	});
-
+		menu_top__show_on_mouse_scroll()
+	})
 
 	// show\hide menu
 	$('.menu__toggle, .menu_slider__close, .page__fade, .topmenu__close').click(function () {
-		$('html').toggleClass('topmenu__active');
+		$html.toggleClass('topmenu__active')
 
-		return false;
-	});
+		return false
+	})
 
 	// for mobile
 	$('.menu_top__toggle_wrap').click(function() {
-		$('html').toggleClass('menu_top__active');
+		$html.toggleClass('menu_top__active')
 
 		// for mobile special
-		$('.topmenu__wrap').focus();
-	});
-
-
+		$('.topmenu__wrap').focus()
+	})
 
 	// when menu opened and user leaving menu
 	$('.topmenu__wrap2').mouseleave(function() {
-		$('html').removeClass('topmenu__active');
-	});
-
-
+		$html.removeClass('topmenu__active')
+	})
 
 	// menu_top__show_on_mouse_scroll [START]
-	var iScrollPos = 0;
+	let iScrollPos = 0
 
 	function menu_top__show_on_mouse_scroll() {
-		var iCurScrollPos = $(this).scrollTop();
+		let iCurScrollPos = $(this).scrollTop()
+		const $html = $('html')
 
 		// scroll down
 		if (iCurScrollPos > iScrollPos && $(window).scrollTop() > 80 ) {
-			$('html').addClass('menu_top__hide');
+			$html.addClass('menu_top__hide')
 		}
 		// scroll up
 		else {
-			if ( $('html').hasClass('menu_top__hide') ) {
-				$('html').removeClass('menu_top__hide');
+			if ($html.hasClass('menu_top__hide')) {
+				$html.removeClass('menu_top__hide')
 			}
 		}
 
-		iScrollPos = iCurScrollPos;
+		iScrollPos = iCurScrollPos
 	}
 	// menu_top__show_on_mouse_scroll [END]
-});
-
-
+})
